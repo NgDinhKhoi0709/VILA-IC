1d06745c7cd0f582b9c5fb84dc785c334e9d8935
NgDinhKhoi <147326561+NgDinhKhoi0709@users.noreply.github.com>
2024-12-28 22:44:38 +0700

    Update video_captioning.py

--- video_captioning.py
+++ video_captioning.py
@@ -3,7 +3,7 @@ import json
 from natsort import natsorted
 from llava.eval.run_vila import main, load_model_once
 
-base_folder = '/kaggle/working/data'
+base_folder = '/kaggle/working/input_vid'
 output_folder = '/kaggle/working/output'
 model_path = 'Efficient-Large-Model/VILA1.5-3b'
 conv_mode = 'vicuna_v1'
