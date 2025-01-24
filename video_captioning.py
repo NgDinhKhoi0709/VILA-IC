0f49908b5997b4a0839f7d9417c1077f6bcdc767
Tung Le Thanh <139512238+Zhennor@users.noreply.github.com>
2025-01-25 01:20:53 +0700

    Update video_captioning.py

--- video_captioning.py
+++ video_captioning.py
@@ -7,7 +7,7 @@ base_folder = '/kaggle/input/festival-fol3'
 output_folder = '/kaggle/working/output'
 model_path = 'Efficient-Large-Model/VILA1.5-3b'
 conv_mode = 'vicuna_v1'
-query = "<video>\n Please describe the video in detail, focusing on the setting, activities, traditional costumes, decorations, cultural symbols, and any performances or rituals that indicate a festival or celebration. Pay special attention to the traditional costumes, describing their colors, patterns, materials, shapes, and any accessories or significant details."
+query = "<video>\n Please describe the video in detail, focusing on the setting, activities, traditional costumes, decorations, cultural symbols, and any performances or rituals that indicate a festival or celebration."
 
 tokenizer, model, image_processor = load_model_once(model_path, conv_mode)
 
